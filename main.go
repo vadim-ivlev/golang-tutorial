51ffb0a0dbeabc94c362a595a48e24119745f9a7
Vadim <vadim.ivlev@gmail.com>
2021-01-27 13:32:06 +0000

    second
    Signed-off-by: Vadim <vadim.ivlev@gmail.com>

--- main.go
+++ main.go
@@ -1,8 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main() {
-	fmt.Println("Привет")
+type person struct {
+	firstName string
+	secondName string
 }
 
+type manager struct {
+	person
+	position string
+}
+
+func (p person) String() string{
+	return fmt.Sprintf(`
+	Имя:     %s  
+	Фамилия: %s
+	`, p.firstName, p.secondName)
+}
+
+
+
+func main() {
+	var p person = person{
+		firstName: "vadim",
+		secondName: "ivlev",
+	}
+	fmt.Println("Привет", math.Pi, p)
+
+	var num0 float64
+	var num1 float64
+	fmt.Print("Enter the numbers:")
+	fmt.Scanf("%f %f",&num0, &num1)
+	fmt.Printf("Numbers %.5f %.5f", num0,  num1)
+
+}
